3b9352effc99ea5e2f94ee6fba15b9f2ee23289b
wnsdud0721 <wnsdud0721@naver.com>
2023-05-24 10:28:08 +0900

    Fix: chatGPT model&token

--- Coava/serverConnectViewController.swift
+++ Coava/serverConnectViewController.swift
@@ -7,98 +7,167 @@
 
 import UIKit
 import Alamofire
+import Speech
+import AVFoundation
 
-class serverConnectViewController: UIViewController {
+class serverConnectViewController: UIViewController, SFSpeechRecognizerDelegate {
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ko-KR"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        speechRecognizer?.delegate = self
     }
     
-    @IBOutlet var currentTime: UILabel!
+
+    @IBOutlet var questionTextField: UITextField!
+    @IBOutlet var responseTextField: UITextField!
+    @IBOutlet var post: UIButton!
     
-    @IBOutlet var userID: UITextField!
+    let synthesizer = AVSpeechSynthesizer()
     
-    @IBOutlet var name: UITextField!
-    
-    @IBOutlet var responseView: UITextView!
-    
-    
-    @IBAction func callCurrentTime(_ sender: Any) {
-       
-    }
+    let apiKey = "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"
+    let baseURL = "https://api.openai.com/v1/completions"
     
     @IBAction func post(_ sender: Any) {
         
-        // 1. 전송할 값 준비
-        let content = (self.userID.text)!
-        let model = "gpt-3.5-turbo"
-        let role = "user"
-        let param = ["role": role, "content": content]
-        let paramData = try! JSONSerialization.data(withJSONObject: param, options: [])
+        if audioEngine.isRunning { // 현재 음성인식이 수행중이라면
+            audioEngine.stop() // 오디오 입력을 중단한다.
+            recognitionRequest?.endAudio() // 음성인식 역시 중단
+            post.isEnabled = false
+            post.setTitle("말하기!", for: .normal)
+        }
+        else {
+            startRecording()
+            post.setTitle("말하기 멈추기", for: .normal)
+        }
         
-        let url = URL(string: "https://api.openai.com/v1/chat/completions")
-        
-        var request = URLRequest(url: url!)
-        request.httpMethod = "POST"
-        request.httpBody = paramData
-        
-        // 4. HTTP 메세지 헤더 설정
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue(String(paramData.count), forHTTPHeaderField: "Content-Length")
-        
-        // 5. URLSession 객체를 통해 전송 및 응답값 처리 로직 작성
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
-            // 5-1. 서버가 응답이 없거나 통신이 실패했을 때
-            if let e = error {
-                NSLog("An error has occured : \(e.localizedDescription)")
-                return
-            }
-            print("Response Data=\(String(data: data!, encoding: .utf8)!)")
-            
-            // 5-2. 응답 처리 로직이 여기에 들어감
-            // 1) 메인 스레드에서 비동기로 처리되도록 한다.
-            DispatchQueue.main.async() {
-                do {
-                    let object = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
-                    guard let jsonObject = object else { return }
+        guard let question = questionTextField.text, !question.isEmpty else {
+            responseTextField.text = "Please enter a question."
+            return
+        }
+                
+        sendChatRequest(prompt: question) { [weak self] (response, error) in
+            guard let self = self else { return }
                     
-                    // 2) JSON 결과값을 추출한다.
-                    let result = jsonObject["result"] as? String
-                    //let timestamp = jsonObject["timestamp"] as? String
-                    let role = jsonObject["role"] as? String
-                    let content = jsonObject["content"] as? String
-                    
-                    // 3) 결과가 성공일 때에만 텍스트 뷰에 출력한다.
-                    if result == "SUCCESS" {
-                        self.responseView.text = "GPT : \(role!)" + "\n"
-                        + "content : \(content!)" + "\n"
-                    }
-                } catch let e as NSError {
-                    print ("An error has occured while parsing JSONObject : \(e.localizedDescription)")
+            DispatchQueue.main.async {
+                if let error = error {
+                    self.responseTextField.text = "Error: \(error)"
+                } else if let response = response {
+                    self.responseTextField.text = response
+                    self.questionTextField.text = nil
+                    self.textToSpeech()
                 }
             }
         }
-        // 6. POST 전송
-        task.resume()
+    }
+    
+    func startRecording() {
+            
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+            
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.record)
+            try audioSession.setMode(AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        }
+        catch {}
+          
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+            
+        let inputNode = audioEngine.inputNode
+               
+        guard let recognitionRequest = recognitionRequest else {
+                fatalError("")
+        }
+            
+        recognitionRequest.shouldReportPartialResults = true
+            
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+                
+            var isFinal = false
+                
+            if result != nil {
+                print("변환된 음성 : ",result?.bestTranscription.formattedString as Any)
+                self.questionTextField.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            if isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                                
+                self.post.isEnabled = true
+            }
+        })
+                        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+                        
+        audioEngine.prepare()
+                        
+        do {
+            try audioEngine.start()
+        }
+        catch {}
+    }
+    
+    func textToSpeech() {
         
+        let utterance = AVSpeechUtterance(string: responseTextField.text!)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+        utterance.rate = 0.4
+        synthesizer.speak(utterance)
+        
+    }
+    
+    func sendChatRequest(prompt: String, completionHandler: @escaping (String?, Error?) -> Void) {
+        guard let url = URL(string: baseURL) else {
+            completionHandler(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let parameters = ["model": "text-davinci-003", "prompt": prompt, "max_tokens": 250, "temperature": 0.7] as [String : Any]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completionHandler(nil, error)
+                return
+            }
+            
+            if let data = data {
+                if let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+                   let jsonDict = jsonObject as? [String: Any],
+                   let choices = jsonDict["choices"] as? [[String: Any]],
+                   let text = choices.first?["text"] as? String {
+                    completionHandler(text, nil)
+                    return
+                }
+            }
+            completionHandler(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid response"]))
+        }
+        task.resume()
     }
 }
 
-extension Bundle {
-    var chatGPTAPIKey: String {
-        get {
-            guard let filePath = Bundle.main.path(forResource: "Property List", ofType: "plist") else {
-                fatalError("Couldn't find file 'Property List.plist'.")
-            }
-            let plist = NSDictionary(contentsOfFile: filePath)
-            
-            guard let value = plist?.object(forKey: "CHATGPT_KEY") as? String else {
-                fatalError("Couldn't find key 'CHATGPT_KEY' in 'Property List.plist'.")
-            }
-            return value
-        }
-    }
-}
+
